d0f0732f90d4d5e51f6f4c647f16563d8d0f6eaa
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-03-01 00:33:05 +0600

    Enhance App component with styling and additional content; add primary button styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 
 const App = () => {
   return (
-    <div>
-      <h1 className=" text-center bolder text-2xl font-mono">Hello World</h1>
+    <div className="w-full h-screen mx-auto bg-slate-900">
+      <h1 className=" text-[#fff] text-center bolder text-2xl font-mono">
+        Hello World
+      </h1>
+      <p className="text-center text-lg font-sans text-gray-500 mt-4 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptates, voluptatum, quidem, quos quia quae quibusdam doloribus
+        doloremque quod magnam quas. Quae, quibusdam. Fugit, voluptate
+        repudiandae. Quo, natus. Quisquam, quidem.
+      </p>
+
+      <div className=" py-10 px-10 ">
+        <button className=" priamry_btn">Click Me</button>
+      </div>
     </div>
   );
 };
